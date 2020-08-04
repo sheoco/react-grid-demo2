3a544ca9d8a05c25386f5a914f53d958802f236f
Sheo Shankar <sheointime@gmail.com>
2020-08-04 16:13:06 +0530

    Updates

--- precache-manifest.e6c1890c1ccb040e5674c87964c379e0.js
+++ precache-manifest.e6c1890c1ccb040e5674c87964c379e0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "082490045a73939702c64a3d590e6993",
+    "revision": "f51f40a652870f2750879e7063aa7070",
     "url": "/react-grid-demo2/index.html"
   },
   {
-    "revision": "41861452f5156f9baa46",
-    "url": "/react-grid-demo2/static/css/main.85863738.chunk.css"
+    "revision": "5f4fa430321f76bfaff8",
+    "url": "/react-grid-demo2/static/css/main.b1751437.chunk.css"
   },
   {
-    "revision": "678a97f3ab3acc8335a5",
-    "url": "/react-grid-demo2/static/js/2.3c801315.chunk.js"
+    "revision": "559263aeac3370cb3ca6",
+    "url": "/react-grid-demo2/static/js/2.5b82b457.chunk.js"
   },
   {
     "revision": "2fa605965a9d56ccc6b0e840a98e376e",
-    "url": "/react-grid-demo2/static/js/2.3c801315.chunk.js.LICENSE.txt"
+    "url": "/react-grid-demo2/static/js/2.5b82b457.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "41861452f5156f9baa46",
-    "url": "/react-grid-demo2/static/js/main.cd2003d6.chunk.js"
+    "revision": "5f4fa430321f76bfaff8",
+    "url": "/react-grid-demo2/static/js/main.a150a856.chunk.js"
   },
   {
     "revision": "a39fb0e780b1c0ac00ec",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-grid-demo2/precache-manifest.70f319fccc5b56e7ddbd5bbf987a5f77.js"
+  "/react-grid-demo2/precache-manifest.e6c1890c1ccb040e5674c87964c379e0.js"
 );
 
 self.addEventListener('message', (event) => {
